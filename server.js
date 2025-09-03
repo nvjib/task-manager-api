f89dfdb9a0a8b2ec52b90b69126462e90d30cdf3
nvjib <nvjibiman@gmail.com>
2025-09-03 23:56:39 +0100

    Add /tasks GET route, connected to Supabase

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
-const express = require("express")
-const app = express()
+require("dotenv").config()
 
+const express = require("express")
+const supabase = require("./db")
+
+const app = express()
 app.use(express.json())
+
+// get all tasks
+app.get("/tasks", async (req, res) => {
+    const { data, error } = await supabase.from("tasks").select("*")
+
+    if (error) {
+        return res.status(500).json({ error: error.message })
+    }
+
+    if (!data || data.length === 0) {
+        return res.status(400).json({ error: "Tasks not found" })
+    }
+
+    return res.status(200).json(data)
+})
 
 app.listen(3000, () => console.log("Server is running on: http://localhost:3000"))
