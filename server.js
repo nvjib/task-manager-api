eb7987ad68591b10200a565c3a51d95f98450bd3
nvjib <nvjibiman@gmail.com>
2025-09-07 16:52:06 +0100

    Add DELETE /tasks endpoint to delete a task

--- server.js
+++ server.js
@@ -64,6 +64,10 @@ app.put("/tasks/:id", async (req, res) => {
     const { title, completed } = req.body
     const { id } = req.params
 
+    if (!id) {
+        return res.status(400).json({ error: "Missing task ID" })
+    }
+
     if (!title.trim() || typeof completed !== "boolean") {
         return res.status(400).json({ error: "Missing Required Fields" })
     }
@@ -93,6 +97,45 @@ app.put("/tasks/:id", async (req, res) => {
     }
 
     return res.status(200).json({ message: "Task updated sucessfully"  })
+})
+
+// delete a task
+app.delete("/tasks/:id", async (req, res) => {
+    // extract id from URL
+    const { id } = req.params
+
+    // id validation
+    if (!id) {
+        return res.status(400).json({ error: "Missing task ID" })
+    }
+
+    // validate id exists in the database
+    const { data, error } = await supabase
+        .from("tasks")
+        .select()
+        .eq("id", id) 
+        .single()
+
+    if (error) {
+        return res.status(500).json({ error: error.message })
+    }
+
+    if (!data) {
+        return res.status(404).json({ error: "Task not found" })
+    }
+
+    // delete task
+    const { error: deleteError } = await supabase
+        .from("tasks")
+        .delete()
+        .eq("id", id)
+        
+
+    if (deleteError) {
+        return res.status(500).json({ error: deleteError })
+    }
+
+    return res.status(200).json({ message: "Task deleted successfully" })
 })
 
 app.listen(3000, () => console.log("Server is running on: http://localhost:3000"))
